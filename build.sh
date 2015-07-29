20761d570ff9c9fc439369f2191dd4d05733cd75
Massimo Candela <mcandela@ripe.net>
2015-07-29 14:23:10 +0200

    clean build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Set your directories here:
 css_location='dev/view/css/'
 css_dist_location='dev/view/css/'
 deployment_dir='/Volumes/PersonalDisk/work/git-workspace/atlas-ui/measurements/static/measurements/widgets/latencymon/'
@@ -8,18 +9,12 @@ if [ "$1" == "-css" ]; then
 rm ${css_dist_location}style-lib-dist.min.css
 rm ${css_dist_location}style-lib-dist.css
 cat ${css_location}/*.css > ${css_location}style-lib-dist.tmp
-#cleancss -o ${css_location}style-lib-dist.min.tmp ${css_location}style-lib-dist.tmp
 cat ${css_location}style-lib-header.tpl ${css_location}style-lib-dist.tmp ${css_location}style-lib-footer.tpl > ${css_location}style-lib-dist.scss
 rm ${css_location}style-lib-dist.tmp
 
 sass ${css_location}style-lib-dist.scss ${css_dist_location}style-lib-dist.css
 rm ${css_location}style-lib-dist.scss
-#lessc ${css_location}style-lib-dist.min.scss ${css_location}style-lib-dist.css
-#cleancss --s0 --skip-aggressive-merging --skip-media-merging --skip-restructuring  -o ${css_dist_location}style-lib-dist.min.css ${css_dist_location}style-lib-dist.css
 minify -o ${css_dist_location}style-lib-dist.min.css ${css_dist_location}style-lib-dist.css
-#mv ${css_location}/images ${css_dist_location}/images
-#mv ${view_location}/fonts ${dist_location}/fonts
-#mv ${view_location}/img ${dist_location}/img
 fi
 
 
