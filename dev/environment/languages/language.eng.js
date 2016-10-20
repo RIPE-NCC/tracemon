b08b45fe19d55ae15cfd1fb8e48a1661a74bcd3a
Massimo Candela <mcandela@ripe.net>
2016-10-20 11:59:00 +0200

    language separation - partial

--- dev/environment/languages/language.eng.js
+++ dev/environment/languages/language.eng.js
@@ -5,6 +5,11 @@
 
 define([], function(){
     return {
+        latency: "Latency",
+        traceroutesDontReach: "Some traceroutes don't reach the target",
+        allTraceroutesDontReach: "All the traceroutes don't reach the target",
+        title: "Traceroute Route Visualisation",
+        hostGroupedBy: "Hosts grouped by",
 
         views: {
             host: "IP",
